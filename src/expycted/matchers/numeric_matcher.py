25c6f83576b2baef01f1d9e9835f30611f14001d
Dov Benyomin Sohacheski <b@kloud.email>
2023-01-09 10:34:25 +0200

    Update numeric_matcher.py

--- src/expycted/matchers/numeric_matcher.py
+++ src/expycted/matchers/numeric_matcher.py
@@ -7,13 +7,11 @@ class NumericMatcher(BaseMatcher):
     """Asserts that the value is numeric."""
 
     def _matches(self, *, expected) -> bool:
-        if type(self._expectation.actual) is str:
+        if type(self._expectation.actual) is not str:
+            return type(self._expectation.actual) in [int, float]
 
-            try:
-                float(self._expectation.actual)
-                return True
-
-            except Exception:
-                return False
-
-        return type(self._expectation.actual) in [int, float]
+        try:
+            float(self._expectation.actual)
+            return True
+        except ValueError:
+            return False
